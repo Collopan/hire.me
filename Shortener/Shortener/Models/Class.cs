f8a09f33ee08fb8a4ee37b864e2af3170e6b6efa
Renan <renancollopy@gmail.com>
2020-03-02 14:02:56 -0300

    Primeiro commit.

---
+++ Shortener/Shortener/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shortener.Models
+{
+    public class Class
+    {
+        public string _URL { get; set; }
+        public int Time_URL { get; set; }
+        public string Short_URL { get; set; }
+        public string Adicionar(string _URL)
+        {
+            return _URL + "qwes";
+        }
+    }
+    
+
+}
